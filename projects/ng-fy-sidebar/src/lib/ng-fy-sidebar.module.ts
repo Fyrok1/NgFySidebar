dd97f86a7ec41cb6ca3c6a4f95fc98113a0ccaa3
Tahsin CESUR <tahsincesur1@hotmail.com>
2019-10-24 13:40:49 +0300

    v0.0.11

--- projects/ng-fy-sidebar/src/lib/ng-fy-sidebar.module.ts
+++ projects/ng-fy-sidebar/src/lib/ng-fy-sidebar.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { NgFySidebarComponent } from './ng-fy-sidebar.component';
 import { NgFySidebarItemComponent } from './components/ng-fy-sidebar-item/ng-fy-sidebar-item.component';
-import { RippleDirective } from './directives/ripple.directive';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { clickOutsideDirective } from './directives/clickOutside.directive';
+import { NgFyRippleModule } from 'ng-fy-ripple';
 
 @NgModule({
   declarations: [
     NgFySidebarComponent,
     NgFySidebarItemComponent,
-    RippleDirective,
     clickOutsideDirective
   ],
   imports: [
     CommonModule,
+    NgFyRippleModule
   ],
   exports: [NgFySidebarComponent]
 })
